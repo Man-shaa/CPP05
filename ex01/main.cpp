5fbd4cd6a90e31a5679d5ba8f3cc5c7453ea6b99
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-15 22:34:18 +0200

    [ADD] [FINISH] ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 20:28:54 by msharifi          #+#    #+#             */
+/*   Updated: 2023/05/15 22:34:03 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	// {
+	// 	try
+	// 	{
+	// 		Form	tooHigh("Petition pour virer Shanley", 150, 150);
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << std::endl;
+	// 		return (1);
+	// 	}
+	// }
+	{
+		Form		F1("Petition pour virer Shanley le predateur", 149, 150);
+		Bureaucrat	B1("Naelle", 3);
+		Bureaucrat	Loser("Only a loser", 150);
+
+		std::cout << B1 << std::endl;
+		std::cout << F1 << std::endl;
+
+		B1.signForm(F1);
+
+		Loser.signForm(F1);
+	}
+	return (0);
+}
